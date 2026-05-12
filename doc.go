aded1550785c16c95423a73d9f0f73e65401fcc7
Blake Mizerany <blake.mizerany@gmail.com>
2026-05-11 18:23:54 -0700

    unreturned: fold command docs into main

--- doc.go
+++
@@ -1,2 +0,0 @@
-// Command unreturned reports loop-assigned results read after the loop exits.
-package main
--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Command unreturned reports loop-assigned results read after the loop exits.
 package main
 
 import (
